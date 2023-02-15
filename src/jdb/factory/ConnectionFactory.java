15d9d1d06b9b899ab03e33d36fa28e78a63adac9
nela <masham1860@gmail.com>
2023-02-15 11:10:49 -0300

    detalles

--- src/jdb/factory/ConnectionFactory.java
+++ src/jdb/factory/ConnectionFactory.java
@@ -1,7 +1,13 @@
 package jdb.factory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 import javax.sql.DataSource;
 
@@ -10,12 +16,27 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 public class ConnectionFactory {
 
 	public DataSource dataSource;
+	
 
 	public ConnectionFactory() {
+
+	    Properties prop = new Properties();
+	    InputStream input = null;
+	    
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel_alura?useTimezone=true&serverTimezone=UTC");
-		comboPooledDataSource.setUser("root");
-		comboPooledDataSource.setPassword("HA785tyd");
+		
+		try {
+			input = new FileInputStream(".dbconfig.properties");
+			prop.load(input);
+
+			comboPooledDataSource.setJdbcUrl(prop.getProperty("db.url"));
+			comboPooledDataSource.setUser(prop.getProperty("db.username"));
+			comboPooledDataSource.setPassword(prop.getProperty("db.password"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 
 		this.dataSource = comboPooledDataSource;
 	}
